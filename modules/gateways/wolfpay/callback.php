ba929c167ab8b294663d2deec1987be73862dff9
黃柏澄(風雨) <wartw@users.noreply.github.com>
2020-02-15 03:42:10 +0800

    Create callback.php

---
+++ modules/gateways/wolfpay/callback.php
@@ -0,0 +1,41 @@
+<?php
+use WHMCS\Database\Capsule;
+include("../../../init.php");
+include("../../../includes/functions.php");
+include("../../../includes/gatewayfunctions.php");
+include("../../../includes/invoicefunctions.php");
+
+$gatewaymodule = "wolfpay";
+$GATEWAY       = getGatewayVariables($gatewaymodule);
+
+if (!$GATEWAY["type"]) die("fail");
+$pay = new Pays($GATEWAY['mchid'], $GATEWAY['key'], $GATEWAY['api']);
+
+$data = $_GET;
+
+$out_trade_no = $data['out_trade_no'];
+
+if ($pay->verify($data)) {
+    if ($data['trade_status'] == 'TRADE_SUCCESS') {
+    
+		$invoiceid = checkCbInvoiceID($out_trade_no, $GATEWAY["name"]);
+    checkCbTransID($out_trade_no);
+	function convert_helper($invoiceid,$amount){
+    $setting = Capsule::table("tblpaymentgateways")->where("gateway",$gatewaymodule)->where("setting","convertto")->first();
+
+    if (empty($setting)){ return $amount; }
+    
+    $data = Capsule::table("tblinvoices")->where("id",$invoiceid)->get()[0];
+    $userid = $data->userid;
+    $currency = getCurrency( $userid );
+
+    return  convertCurrency( $amount , $setting->value  ,$currency["id"] );
+}
+	  $amount = convert_helper( $invoiceid, $fee);
+    addInvoicePayment($invoiceid,$data['trade_no'],trim($amount),$fee,$typess);
+    logTransaction($GATEWAY["name"], $_REQUEST, "Successful");
+    echo 'success';
+    }
+} else {
+    echo '錯誤';
+}
